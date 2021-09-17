452009373d39b24053a46f5616f9994361b83685
sdaghigh <ucontacti2012@gmail.com>
2021-09-17 17:20:41 +0200

    converted auto last 14 days unixtime

--- automation/match_id_collector.py
+++ automation/match_id_collector.py
@@ -1,9 +1,15 @@
 import requests
 import json
 import sys
+import time
+import datetime
 
 if __name__ == "__main__":
+    d = datetime.datetime.now() - datetime.timedelta(days=14)
+    unixtime = time.mktime(d.timetuple())
+
     response = requests.get("https://api.opendota.com/api/players/" + sys.argv[1] + "/matches")
+
     for i in response.json():
-        if i["hero_id"] == 102 and i["start_time"] > 1630673059:
+        if i["hero_id"] == 102 and i["start_time"] > unixtime:
             print(i["match_id"])
