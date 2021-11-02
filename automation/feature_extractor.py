ee1968716ff93ae42e2b40683309bcc84d128544
sdaghigh <ucontacti2012@gmail.com>
2021-11-02 17:45:39 +0100

    the automation data collection now uses relative path

--- automation/feature_extractor.py
+++ automation/feature_extractor.py
@@ -1,19 +1,20 @@
 import subprocess
 import shutil
+import os
 
 def replay_decompress(match_id_list):
     new_val = []
     for match in match_id_list:
         match = str(match)
-        filename = "/media/hap/normal/replays/" + match + ".dem.bz2"
+        filename = "downloaded_replays/" + match + ".dem.bz2"
         try:    
             output = subprocess.run(['bzip2', '-dk', filename],
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                     check=True)
-            shutil.move("/media/hap/normal/replays/" + match + ".dem", "/home/hap/projects/dota2/player_identifier_Dota2/replay_downloader/downloads/" + match + ".dem")
+            shutil.move("downloaded_replays/" + match + ".dem", "downloaded_replays/" + match + ".dem")
             print(output)
         except:
-            new_value.append(-3)
+            new_val.append(-3)
             continue
         new_val.append(2)
     return new_val
@@ -22,14 +23,14 @@ def game_info(match_id_list):
     new_val = []
     for match in match_id_list:
         match = str(match)
-        filename = "/home/hap/projects/dota2/player_identifier_Dota2/replay_downloader/downloads/" + match + ".dem"
+        filename = os.path.abspath("downloaded_replays/" + match + ".dem")
         try:
-            output = subprocess.run(['java', '-jar', '/home/hap/projects/dota2/player_identifier_Dota2/data_collector/target/info.one-jar.jar', filename],         
+            output = subprocess.run(['java', '-jar', 'java/info.one-jar.jar', filename],         
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 check=True)
             print(output)
         except:
-            new_value.append(-4)
+            new_val.append(-4)
             continue
         new_val.append(3)
 
@@ -40,14 +41,14 @@ def unit_order(match_id_list):
     new_val = []
     for match in match_id_list:
         match = str(match)
-        filename = "/home/hap/projects/dota2/player_identifier_Dota2/replay_downloader/downloads/" + match + ".dem"
+        filename = "downloaded_replays/" + match + ".dem"
         try:
-            output = subprocess.run(['java', '-jar', '/home/hap/projects/dota2/player_identifier_Dota2/data_collector/target/unit_orders.one-jar.jar', filename],         
+            output = subprocess.run(['java', '-jar', 'java/unit_orders.one-jar.jar', filename],         
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 check=True)
             print(output)
         except:
-            new_value.append(-5)
+            new_val.append(-5)
             continue
         new_val.append(4)
     return new_val
@@ -57,15 +58,15 @@ def cursor_data(match_id_list, tickrate = 1):
     new_val = []
     for match in match_id_list:
         match = str(match)
-        filename = "/home/hap/projects/dota2/player_identifier_Dota2/replay_downloader/downloads/" + match + ".dem"
+        filename = "downloaded_replays/" + match + ".dem"
         try:
-            output = subprocess.run(['java', '-jar', '/home/hap/projects/dota2/player_identifier_Dota2/data_collector/target/cursor_all.one-jar.jar', filename, str(tickrate)],         
+            output = subprocess.run(['java', '-jar', 'java/cursor_all.one-jar.jar', filename, str(tickrate)],         
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 check=True)
-            shutil.move("/home/hap/projects/dota2/player_identifier_Dota2/automation/features/" + match + "_cursor_tmp.csv", "/home/hap/projects/dota2/player_identifier_Dota2/automation/features/" + match + "_cursor_tmp_"  + tickrate + "_tick.csv")
+            shutil.move("java/features/" + match + "_cursor_tmp.csv", "java/features/" + match + "_cursor_tmp_"  + tickrate + "_tick.csv")
             print(output)
         except:
-            new_value.append(-6)
+            new_val.append(-6)
             continue
         
         new_val.append(5)
