7c3c1245c798674bf24d648e2e3ee40a00d7cc2d
sdaghigh <ucontacti2012@gmail.com>
2022-01-12 18:31:54 +0100

    fixed all cursor data collector

--- data_collector/src/main/java/dota2identifier/cursor_all/Main.java
+++ data_collector/src/main/java/dota2identifier/cursor_all/Main.java
@@ -9,6 +9,7 @@ import skadistats.clarity.model.Entity;
 import skadistats.clarity.model.FieldPath;
 import skadistats.clarity.model.state.EntityState;
 import skadistats.clarity.model.StringTable;
+import skadistats.clarity.model.FieldPath;
 
 import skadistats.clarity.processor.reader.OnMessage;
 import skadistats.clarity.processor.reader.OnTickEnd;
@@ -27,12 +28,13 @@ import skadistats.clarity.processor.entities.OnEntityUpdated;
 import skadistats.clarity.processor.entities.OnEntityUpdatesCompleted;
 import skadistats.clarity.processor.entities.UsesEntities;
 import skadistats.clarity.processor.entities.OnEntityPropertyChanged;
+import skadistats.clarity.event.Insert;
+import skadistats.clarity.io.Util;
 
 import skadistats.clarity.processor.stringtables.StringTables;
 import skadistats.clarity.processor.stringtables.UsesStringTable;
 
-import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.*;
 import java.io.File;
 import java.io.PrintWriter;
 
@@ -41,6 +43,10 @@ public class Main {
 
     private final Logger log = LoggerFactory.getLogger(Main.class.getPackage().getClass());
 
+    public static final int RADIANT = 2;
+    public static final int DIRE = 3;
+    private boolean running = false;
+
     // TODO: Improve hero recognition
     private boolean isHero(Entity e) {
         if (e.getDtClass().getDtName().equals("CDOTA_Unit_Hero_Beastmaster_Hawk"))
@@ -48,6 +54,24 @@ public class Main {
         if (e.getDtClass().getDtName().equals("CDOTA_Unit_Hero_Beastmaster_Boar"))
             return false;
         return e.getDtClass().getDtName().startsWith("CDOTA_Unit_Hero");
+    }
+    public static boolean isPlayer(Entity e) {
+        return e.getDtClass().getDtName().startsWith("CDOTAPlayer");
+    }
+
+    public boolean isGamePlayer(Entity e) {
+    	if (isPlayer(e)) {
+    		int playerTeamNum = getEntityProperty(e, "m_iTeamNum");
+    		return playerTeamNum == RADIANT || playerTeamNum == DIRE;
+    	}
+    	return false;
+    }
+
+    public static Iterator<Entity> getEntities(Context ctx, String entityName) {
+        if (ctx != null) {
+            return ctx.getProcessor(Entities.class).getAllByDtName(entityName);
+        }
+        return null;
     }
 
     private int tick;
@@ -61,6 +85,8 @@ public class Main {
     private PrintWriter mouse_writer;
     private int tick_rate;
 
+    @Insert
+    private Context ctx;
 
     public String getEntityNameByHandle(int id, Entities entities, StringTable stringTable) {
         Entity entity = entities.getByHandle(id);
@@ -76,24 +102,63 @@ public class Main {
 
     @OnEntityCreated
     public void onCreated(Entity e) {
-        // if(isHero(e))
-        //     if (0 <= Integer.parseInt(e.getProperty("m_iPlayerID"))  && Integer.parseInt(e.getProperty("m_iPlayerID")) <= 9)
-        if(e.getDtClass().getDtName().equals("CDOTAPlayer"))
+        if (running)
         {
-                ent_list.add(e);
+            // if(isHero(e))
+            //     if (0 <= Integer.parseInt(e.getProperty("m_iPlayerID"))  && Integer.parseInt(e.getProperty("m_iPlayerID")) <= 9)
+            if (isHero(e)) {
+                // System.out.print("bruh");
+                Integer id = e.getProperty("m_iPlayerID");
+                heroHashtbl.put(id, e.getDtClass().getDtName());
+                // ent_list.add(e);
+            }     
+            // if(e.getDtClass().getDtName().equals("CDOTAPlayer") && isHero(e))
+            // {
+            //     System.out.println("obj");
+            // }
         }
-        if (isHero(e)) {
-            Integer id = e.getProperty("m_iPlayerID");
-            heroHashtbl.put(id, e.getDtClass().getDtName());
-            // ent_list.add(e);
-        }     
     }
 
+    public static String getTeamName(int team) {
+        switch(team) {
+            case 2: return "Radiant";
+            case 3: return "Dire";
+            default: return "";
+        }
+    }
+
+    public static Entity getEntity(Context ctx, String entityName) {
+        if (ctx != null) {
+            return ctx.getProcessor(Entities.class).getByDtName(entityName);
+        }
+        return null;
+    }
+
+    public static <T> T resolveValue(Context ctx, String entityName, String pattern, int index, int team, int pos) {
+        String fieldPathString = pattern
+                .replaceAll("%i", Util.arrayIdxToString(index))
+                .replaceAll("%t", Util.arrayIdxToString(team))
+                .replaceAll("%p", Util.arrayIdxToString(pos));
+        String compiledName = entityName.replaceAll("%n", getTeamName(team));
+        Entity entity = getEntity(ctx, compiledName);
+        FieldPath fieldPath = entity.getDtClass().getFieldPathForName(fieldPathString);
+
+        return entity.getPropertyForFieldPath(fieldPath);
+    }
+
+    public static <T> T getEntityProperty(Entity e, String property) {
+    	try {
+            FieldPath f = e.getDtClass().getFieldPathForName(property);
+            return e.getPropertyForFieldPath(f);
+        } catch (Exception x) {
+            return null;
+        }
+    }
 
     @UsesEntities
     @UsesStringTable("EntityNames")
     @OnTickStart
-    public void OnTickEnd(Context ctx, boolean synthetic) {
+    public void OnTickStart(Context ctx, boolean synthetic) {
         tick = ctx.getTick();
         if ((tick % tick_rate) == 0)
         {    
@@ -132,6 +197,15 @@ public class Main {
     }
     
     public void run(String[] args) throws Exception {
+        long tStart = System.currentTimeMillis();
+        new SimpleRunner(new MappedFileSource(args[0])).runWith(this);
+        long tMatch = System.currentTimeMillis() - tStart;
+        running = false;
+        log.info("total time taken: {}s", (tMatch) / 1000.0);
+    }
+
+    public void runControlled(String[] args) throws Exception {
+
         String rep_pwd = args[0];
 
         if (args.length == 1)
@@ -165,29 +239,48 @@ public class Main {
         sb.append("Y");
         sb.append('\n');
         mouse_writer.write(sb.toString());
+        running = true;
 
         long tStart = System.currentTimeMillis();
-        new SimpleRunner(new MappedFileSource(args[0])).runWith(this);
-        long tMatch = System.currentTimeMillis() - tStart;
-        log.info("total time taken: {}s", (tMatch) / 1000.0);
-                
-        mouse_writer.close();
-    }
-
-    public void runControlled(String[] args) throws Exception {
-        long tStart = System.currentTimeMillis();
-        ControllableRunner runner = new ControllableRunner(new MappedFileSource(args[0])).runWith(this);
-        while(!runner.isAtEnd()) {
-            runner.tick();
+        ControllableRunner cRunner = new ControllableRunner(new MappedFileSource(args[0])).runWith(this);
+        for (int i = 0 ; i < 30000; i++) {
+            cRunner.tick();
         }
+
+        if (ctx != null) {
+            Iterator<Entity> playerEntities = getEntities(ctx, "CDOTAPlayer");
+            while(playerEntities.hasNext()) {
+                Entity playerEntity = playerEntities.next();
+                if (isGamePlayer(playerEntity))
+                {
+                    ent_list.add(playerEntity);
+                    // int playerID = getEntityProperty(playerEntity, "m_iPlayerID");
+                    // System.out.println(playerID);
+                    // int heroID = resolveValue(ctx, "CDOTA_PlayerResource", "m_vecPlayerTeamData.%i.m_nSelectedHeroID", playerID, 0, 0);
+                    // System.out.println(heroID);
+                    // int selectedHero = resolveValue(ctx, "CDOTA_PlayerResource", "m_vecPlayerTeamData.%i.m_hSelectedHero", playerID, 0, 0);
+                    // System.out.println(selectedHero);
+                    // long steamID =  resolveValue(ctx, "CDOTA_PlayerResource", "m_vecPlayerData.%i.m_iPlayerSteamID", playerID, 0, 0);
+                    // System.out.println(steamID);
+                    // int teamNum = getEntityProperty(playerEntity, "m_iTeamNum");
+                    // System.out.println(teamNum);
+                }
+            }
+        }    
+
+        while(!cRunner.isAtEnd()) {
+            cRunner.tick();
+        }
+        cRunner.halt();
+
         long tMatch = System.currentTimeMillis() - tStart;
-        log.info("total time taken: {}s", (tMatch) / 1000.0);
-        
+        log.info("total time taken: {}s", (tMatch) / 1000.0);    
+        mouse_writer.close();
     }
 
 
     public static void main(String[] args) throws Exception {
-        new Main().run(args);
+        new Main().runControlled(args);
     }
 
 }
