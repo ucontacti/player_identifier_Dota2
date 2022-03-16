04562ed21b381c37d5734079bd744f573cd26413
Sali <ucontacti2012@gmail.com>
2022-03-16 17:34:25 +0100

    finally fixed the 10 sequence none moving cursor

--- better_trainer/atomic_feature_extractor.py
+++ better_trainer/atomic_feature_extractor.py
@@ -7,17 +7,122 @@ from sklearn.model_selection import train_test_split #for split the data
 
 from sklearn.model_selection import cross_validate #score evaluation
 
+# In[1]: Other stuff
+# df_cursor = pd.read_csv("cursor.csv")
+# df_unit_order = pd.read_csv("unit_order.csv")
+# df_unit_order.drop_duplicates(inplace=True)
+# df_unit_order.replace({"Action": {'M': 1, 'A': 2, 'S': 3},}, inplace=True)
+# df_match_info = pd.read_csv("info.csv")
+# dfs_cursor = [rows for _, rows in df_cursor.groupby('Hero')]
+# dfs_unit_order = [rows for _, rows in df_unit_order.groupby('Hero')]
+# for hero in dfs_unit_order:
+#     steam_id = df_match_info.loc[df_match_info["Hero"] == hero["Hero"].iloc[0]].iloc[0]["SteamId"]
+#     hero_name = df_match_info.loc[df_match_info["Hero"] == hero["Hero"].iloc[0]].iloc[0]["Hero"]
+#     hero = hero.groupby(hero['Tick']).aggregate({'Action': 'max'}).reset_index()
+#     df_hero_cursor = list(filter(lambda x: x.iloc[0]["Hero"] == hero_name, dfs_cursor))[0]
+    
+#     df_hero_tmp = df_hero_cursor.drop_duplicates(subset=["X", "Y"], keep="last")
+#     df_hero_tmp["seq"] = df_hero_tmp["Tick"].diff()
+#     tbd_index = pd.cut(df_hero_tmp[df_hero_tmp["seq"] > 10]["Tick"], hero["Tick"].values).drop_duplicates(keep="last").index.values[:-1]
+#     df_hero_cursor["Action"] = pd.cut(df_hero_cursor["Tick"], bins=hero["Tick"].values, labels=hero["Action"].iloc[1:].values, ordered=False)
+#     df_hero_cursor["Action"] = pd.to_numeric(df_hero_cursor["Action"])
+#     df_hero_cursor["range"] = pd.cut(df_hero_cursor["Tick"], hero["Tick"].values)
+#     print(df_hero_cursor.size)
+#     for rm in tbd_index:
+#         df_hero_cursor.drop( df_hero_cursor[(df_hero_cursor.loc[rm]["range"].left <= df_hero_cursor["Tick"]) & (df_hero_cursor["Tick"] <= df_hero_cursor.loc[rm]["Tick"])].index, inplace=True)
+#     print(df_hero_cursor.size)
+#     df_hero_cursor.dropna(inplace=True)
+    # df_hero_cursor["V_X"] = df_hero_cursor["X"].diff() / df_hero_cursor["Tick"].diff()
+    # df_hero_cursor["V_Y"] = df_hero_cursor["Y"].diff() / df_hero_cursor["Tick"].diff()
+    # df_hero_cursor["V"] = np.sqrt(df_hero_cursor["V_X"]**2 + df_hero_cursor["V_Y"]**2)
+    # df_hero_cursor["S"] = np.sqrt((df_hero_cursor["X"].diff())**2 + (df_hero_cursor["Y"].diff())**2)
+    # df_hero_cursor.fillna({"S":0}, inplace=True)
+    # df_hero_cursor["S"] = np.cumsum(df_hero_cursor["S"])
+    # df_hero_cursor["A"] = df_hero_cursor["V"].diff() / df_hero_cursor["Tick"].diff()
+    # df_hero_cursor["J"] = df_hero_cursor["A"].diff() / df_hero_cursor["Tick"].diff()
+    # df_hero_cursor["AoM"] = np.arctan(df_hero_cursor["Y"].diff() / df_hero_cursor["X"].diff())
+    # df_hero_cursor.fillna({"AoM":0}, inplace=True)
+    # df_hero_cursor["AoM"] = np.cumsum(df_hero_cursor["AoM"])
+    # df_hero_cursor["Ang_V"] = df_hero_cursor["AoM"].diff() / df_hero_cursor["Tick"].diff()
+    # df_hero_cursor["Cur"] = df_hero_cursor["AoM"].diff() / df_hero_cursor["S"].diff()
+    # df_hero_cursor["Y_diff"] = df_hero_cursor["Y"].diff()
+    # df_hero_cursor["X_diff"] = df_hero_cursor["X"].diff()
+    # # df_hero_cursor["Cur_cr"] = df_hero_cursor["Cur"].diff() / df_hero_cursor["S"].diff()
+    # df_hero_cursor.fillna({"V_X":0, "V_Y":0, "V":0, "A":0, "J":0, "AoM":0, "Ang_V":0, "Cur":0, "Y_diff":0, "X_diff":0}, inplace=True)
+    # atomic_order = df_hero_cursor.groupby("range").agg(
+    #     Tick=("Tick", "count"), 
+    #     Action=("Action", "first"),
+    #     X_diff=("X_diff", "sum"),
+    #     Y_diff=("Y_diff", "sum"),
+    #     V_X_min=("V_X", "min"),
+    #     V_X_max=("V_X", "max"),
+    #     V_X_mean=("V_X", "mean"),
+    #     V_X_std=("V_X", "std"),
+    #     V_Y_min=("V_Y", "min"),
+    #     V_Y_max=("V_Y", "max"),
+    #     V_Y_mean=("V_Y", "mean"),
+    #     V_Y_std=("V_Y", "std"),
+    #     V_min=("V", "min"),
+    #     V_max=("V", "max"),
+    #     V_mean=("V", "mean"),
+    #     V_std=("V", "std"),
+    #     A_min=("A", "min"),
+    #     A_max=("A", "max"),
+    #     A_mean=("A", "mean"),
+    #     A_std=("A", "std"),
+    #     J_min=("J", "min"),
+    #     J_max=("J", "max"),
+    #     J_mean=("J", "mean"),
+    #     J_std=("J", "std"),
+    #     AoM_min=("AoM", "min"),
+    #     AoM_max=("AoM", "max"),
+    #     AoM_mean=("AoM", "mean"),
+    #     AoM_std=("AoM", "std"),
+    #     Ang_V_min=("Ang_V", "min"),
+    #     Ang_V_max=("Ang_V", "max"),
+    #     Ang_V_mean=("Ang_V", "mean"),
+    #     Ang_V_std=("Ang_V", "std"),
+    #     Cur_min=("Cur", "min"),
+    #     Cur_max=("Cur", "max"),
+    #     Cur_mean=("Cur", "mean"),
+    #     Cur_std=("Cur", "std")
+    #     ).fillna(0).replace([np.inf, -np.inf], 0)
+    
+    # atomic_order["distance"] = np.sqrt(atomic_order["X_diff"]**2 + atomic_order["Y_diff"]**2)
+    # atomic_order.drop("X_diff", axis=1 ,inplace=True)
+    # atomic_order.drop("Y_diff", axis=1 ,inplace=True)
+    # atomic_order.drop(atomic_order[atomic_order["Tick"] < 8].index, inplace = True)
+    # atomic_order["Steam_id"] = steam_id
+    # atomic_order["Hero"] = hero_name
+    
+
+
 # In[2]: Complex mouse action
-from replay_ids import authentic_match_id
+authentic_match_id = replay_tracker.loc[(replay_tracker['state'] == 6) & (replay_tracker['1_tick'] == True), 'replay_id'].tolist()
 
 counter = 1
 
+not_yet = True
+
 for match_id in authentic_match_id:
-    df_cursor = pd.read_csv("../data_collector/features/" + match_id + "_cursor_tmp.csv")
-    df_unit_order = pd.read_csv("../data_collector/features/" + match_id + "_unit_order_v2.csv")
+    print(match_id)
+    match_id = str(match_id)
+    if match_id != "6301239781":
+        if not_yet:
+            counter += 1
+            continue
+    else:
+        not_yet = False
+        print("lets go")
+        counter += 1
+        continue
+    df_cursor = pd.read_csv("../automation/features/" + match_id + "_cursor_tmp_1_tick.csv")
+    df_cursor = df_cursor[df_cursor['Tick'] > 29999]
+    df_unit_order = pd.read_csv("../automation/features/"  + match_id + "_unit_order_v2.csv")
+    df_unit_order = df_unit_order[df_unit_order['Tick'] > 29999]
     df_unit_order.drop_duplicates(inplace=True)
     df_unit_order.replace({"Action": {'M': 1, 'A': 2, 'S': 3},}, inplace=True)
-    df_match_info = pd.read_csv("../data_collector/features/" + match_id + "_info.csv")
+    df_match_info = pd.read_csv("../automation/features/"  + match_id + "_info.csv")
 
     dfs_cursor = [rows for _, rows in df_cursor.groupby('Hero')]
     dfs_unit_order = [rows for _, rows in df_unit_order.groupby('Hero')]
@@ -25,10 +130,26 @@ for match_id in authentic_match_id:
         steam_id = df_match_info.loc[df_match_info["Hero"] == hero["Hero"].iloc[0]].iloc[0]["SteamId"]
         hero_name = df_match_info.loc[df_match_info["Hero"] == hero["Hero"].iloc[0]].iloc[0]["Hero"]
         hero = hero.groupby(hero['Tick']).aggregate({'Action': 'max'}).reset_index()
-        df_hero_cursor = list(filter(lambda x: x.iloc[0]["Hero"] == hero_name, dfs_cursor))[0]
+        try:
+            df_hero_cursor = list(filter(lambda x: x.iloc[0]["Hero"] == hero_name, dfs_cursor))[0]
+        except:
+            continue
+
+        #df_hero_cursor.drop_duplicates(subset=["X", "Y"], keep="last", inplace=True)
+        #df_hero_cursor["seq"] = df_hero_cursor["Tick"].diff()
+        #df_hero_cursor.drop(df_hero_cursor[df_hero_cursor["seq"] > 10].index, inplace=True)
+        #df_hero_cursor.drop("seq", axis=1, inplace=True)
+
+        df_hero_tmp = df_hero_cursor.drop_duplicates(subset=["X", "Y"], keep="last")
+        df_hero_tmp["seq"] = df_hero_tmp["Tick"].diff()
+        tbd_index = pd.cut(df_hero_tmp[df_hero_tmp["seq"] > 10]["Tick"], hero["Tick"].values).drop_duplicates(keep="last").index.values[:-1]
         df_hero_cursor["Action"] = pd.cut(df_hero_cursor["Tick"], bins=hero["Tick"].values, labels=hero["Action"].iloc[1:].values, ordered=False)
         df_hero_cursor["Action"] = pd.to_numeric(df_hero_cursor["Action"])
         df_hero_cursor["range"] = pd.cut(df_hero_cursor["Tick"], hero["Tick"].values)
+        for rm in tbd_index:
+            if isinstance(df_hero_cursor.loc[rm]["range"], float):
+                continue
+            df_hero_cursor.drop( df_hero_cursor[(df_hero_cursor.loc[rm]["range"].left <= df_hero_cursor["Tick"]) & (df_hero_cursor["Tick"] <= df_hero_cursor.loc[rm]["Tick"])].index, inplace=True)
         df_hero_cursor.dropna(inplace=True)
         df_hero_cursor["V_X"] = df_hero_cursor["X"].diff() / df_hero_cursor["Tick"].diff()
         df_hero_cursor["V_Y"] = df_hero_cursor["Y"].diff() / df_hero_cursor["Tick"].diff()
@@ -93,6 +214,9 @@ for match_id in authentic_match_id:
         atomic_order["Steam_id"] = steam_id
         atomic_order["Hero"] = hero_name
         # df_hero_cursor.drop("S", axis=0, inplace=True)
+        if (atomic_order[atomic_order["Action"] == 1].index.empty or atomic_order[atomic_order["Action"] == 2].index.empty or atomic_order[atomic_order["Action"] == 3].index.empty):
+                 print("Empty action")
+                 break
         atomic_order_move = atomic_order.drop(atomic_order[atomic_order["Action"] != 1].index)
         atomic_order_attack = atomic_order.drop(atomic_order[atomic_order["Action"] != 2].index)
         atomic_order_spell = atomic_order.drop(atomic_order[atomic_order["Action"] != 3].index)
--- data_collector/src/main/java/dota2identifier/unit_orders/Main.java
+++ data_collector/src/main/java/dota2identifier/unit_orders/Main.java
@@ -20,6 +20,7 @@ import skadistats.clarity.processor.runner.ControllableRunner;
 import skadistats.clarity.processor.runner.SimpleRunner;
 import skadistats.clarity.source.MappedFileSource;
 
+
 import skadistats.clarity.processor.entities.OnEntityCreated;
 import skadistats.clarity.processor.entities.OnEntityDeleted;
 import skadistats.clarity.processor.entities.OnEntityPropertyCountChanged;
@@ -27,6 +28,9 @@ import skadistats.clarity.processor.entities.OnEntityUpdated;
 import skadistats.clarity.processor.entities.OnEntityUpdatesCompleted;
 import skadistats.clarity.processor.entities.UsesEntities;
 import skadistats.clarity.processor.entities.OnEntityPropertyChanged;
+import skadistats.clarity.event.Insert;
+import skadistats.clarity.io.Util;
+
 
 import skadistats.clarity.processor.stringtables.StringTables;
 import skadistats.clarity.processor.stringtables.UsesStringTable;
@@ -34,14 +38,18 @@ import skadistats.clarity.processor.stringtables.UsesStringTable;
 import skadistats.clarity.wire.common.proto.DotaUserMessages;
 
 
-import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.*;
 import java.io.File;
 import java.io.PrintWriter;
 
 public class Main {
 
     private final Logger log = LoggerFactory.getLogger(Main.class.getPackage().getClass());
+
+    public static final int RADIANT = 2;
+    public static final int DIRE = 3;
+    private boolean running = false;
+
 
     // TODO: Improve hero recognition
     private boolean isHero(Entity e) {
@@ -52,15 +60,27 @@ public class Main {
         return e.getDtClass().getDtName().startsWith("CDOTA_Unit_Hero");
     }
 
-    private int tick;
-    private int item_updater_tick;
-    private int cursor_updater_tick;
-    private String update_hero_name;
-    private int update_hero_id;
-    private int count;
-    private ArrayList<Entity> ent_list=new ArrayList<Entity>();
-    private Hashtable<Integer, String> heroHashtbl = new Hashtable<Integer, String>();;
-    private PrintWriter action_writer;
+    public static boolean isPlayer(Entity e) {
+        return e.getDtClass().getDtName().startsWith("CDOTAPlayer");
+    }
+
+    public boolean isGamePlayer(Entity e) {
+    	if (isPlayer(e)) {
+    		int playerTeamNum = getEntityProperty(e, "m_iTeamNum");
+    		return playerTeamNum == RADIANT || playerTeamNum == DIRE;
+    	}
+    	return false;
+    }
+
+    public static Iterator<Entity> getEntities(Context ctx, String entityName) {
+        if (ctx != null) {
+            return ctx.getProcessor(Entities.class).getAllByDtName(entityName);
+        }
+        return null;
+    }
+
+    @Insert
+    private Context ctx;
 
     public String getEntityNameByHandle(int id, Entities entities, StringTable stringTable) {
         Entity entity = entities.getByHandle(id);
@@ -74,62 +94,120 @@ public class Main {
         }
     }
 
+    public static String getTeamName(int team) {
+        switch(team) {
+            case 2: return "Radiant";
+            case 3: return "Dire";
+            default: return "";
+        }
+    }
+
+    public static Entity getEntity(Context ctx, String entityName) {
+        if (ctx != null) {
+            return ctx.getProcessor(Entities.class).getByDtName(entityName);
+        }
+        return null;
+    }
+
+    public static <T> T resolveValue(Context ctx, String entityName, String pattern, int index, int team, int pos) {
+        String fieldPathString = pattern
+                .replaceAll("%i", Util.arrayIdxToString(index))
+                .replaceAll("%t", Util.arrayIdxToString(team))
+                .replaceAll("%p", Util.arrayIdxToString(pos));
+        String compiledName = entityName.replaceAll("%n", getTeamName(team));
+        Entity entity = getEntity(ctx, compiledName);
+        FieldPath fieldPath = entity.getDtClass().getFieldPathForName(fieldPathString);
+
+        return entity.getPropertyForFieldPath(fieldPath);
+    }
+
+    public static <T> T getEntityProperty(Entity e, String property) {
+    	try {
+            FieldPath f = e.getDtClass().getFieldPathForName(property);
+            return e.getPropertyForFieldPath(f);
+        } catch (Exception x) {
+            return null;
+        }
+    }
+
+
+
+    private int tick;
+    private int item_updater_tick;
+    private int cursor_updater_tick;
+    private String update_hero_name;
+    private int update_hero_id;
+    private int count;
+    private ArrayList<Entity> ent_list=new ArrayList<Entity>();
+    private Hashtable<Integer, String> heroHashtbl = new Hashtable<Integer, String>();;
+    private PrintWriter action_writer;
+
+
     @OnEntityCreated
     public void onCreated(Entity e) {
         // if(isHero(e))
         //     if (0 <= Integer.parseInt(e.getProperty("m_iPlayerID"))  && Integer.parseInt(e.getProperty("m_iPlayerID")) <= 9)
-        if(e.getDtClass().getDtName().equals("CDOTAPlayer"))
-        {
-                ent_list.add(e);
-        }
+        // if(e.getDtClass().getDtName().equals("CDOTAPlayer"))
+        // {
+        //         ent_list.add(e);
+        // }
         if (isHero(e)) {
             Integer id = e.getProperty("m_iPlayerID");
             heroHashtbl.put(id, e.getDtClass().getDtName());
-            ent_list.add(e);
+            // ent_list.add(e);
         }     
     }
 
-    @UsesStringTable("EntityNames")
-    @UsesEntities
+    // @UsesStringTable("EntityNames")
+    // @UsesEntities
     @OnMessage(DotaUserMessages.CDOTAUserMsg_SpectatorPlayerUnitOrders.class)
     public void onMessage(Context ctx, DotaUserMessages.CDOTAUserMsg_SpectatorPlayerUnitOrders message) 
     {
         Entity et = ctx.getProcessor(Entities.class).getByIndex(message.getEntindex()); 
-        StringTable stringTable = ctx.getProcessor(StringTables.class).forName("EntityNames");
-        StringBuilder sb = new StringBuilder();
-        switch (message.getOrderType()) {
-        case 1: // move action
-        case 2: 
-            sb.append(ctx.getTick());
-            sb.append(',');
-            sb.append(heroHashtbl.get(et.getProperty("m_iPlayerID")));
-            sb.append(',');
-            sb.append('M');
-            sb.append('\n');
-            action_writer.write(sb.toString());
-            break;
-        case 3: // attack action
-        case 4:
-            sb.append(ctx.getTick());
-            sb.append(',');
-            sb.append(heroHashtbl.get(et.getProperty("m_iPlayerID")));
-            sb.append(',');
-            sb.append('A');
-            sb.append('\n');
-            action_writer.write(sb.toString());
-            break;
-        case 5: // cast spell action
-        case 6:
-        // case 8:
-            sb.append(ctx.getTick());
-            sb.append(',');
-            sb.append(heroHashtbl.get(et.getProperty("m_iPlayerID")));
-            sb.append(',');
-            sb.append('S');
-            sb.append('\n');
-            action_writer.write(sb.toString());
-            break;
-        }    
+        
+        if (ent_list.contains(et))
+        {
+            if(ctx.getTick() == 32902)
+            {    System.out.println(heroHashtbl.get(et.getProperty("m_iPlayerID")));
+                if (isGamePlayer(et))
+                    System.out.println("daimm");
+            }
+            StringTable stringTable = ctx.getProcessor(StringTables.class).forName("EntityNames");
+            StringBuilder sb = new StringBuilder();
+            switch (message.getOrderType()) {
+            case 1: // move action
+            case 2: 
+                sb.append(ctx.getTick());
+                sb.append(',');
+                sb.append(heroHashtbl.get(et.getProperty("m_iPlayerID")));
+                sb.append(',');
+                sb.append('M');
+                sb.append('\n');
+                action_writer.write(sb.toString());
+                break;
+            case 3: // attack action
+            case 4:
+                sb.append(ctx.getTick());
+                sb.append(',');
+                sb.append(heroHashtbl.get(et.getProperty("m_iPlayerID")));
+                sb.append(',');
+                sb.append('A');
+                sb.append('\n');
+                action_writer.write(sb.toString());
+                break;
+            case 5: // cast spell action
+            case 6:
+            // case 8:
+                sb.append(ctx.getTick());
+                sb.append(',');
+                sb.append(heroHashtbl.get(et.getProperty("m_iPlayerID")));
+                sb.append(',');
+                sb.append('S');
+                sb.append('\n');
+                action_writer.write(sb.toString());
+                break;
+            }    
+        }
     //     if (message.hasAbilityId())
     //     {
     // //         log.info("orderType: {}, hasTargetIndex: {}, hasTargetIndex: {}, hasAbilityId: {}, hasPostion: {}",
@@ -150,6 +228,13 @@ public class Main {
 
 
     public void run(String[] args) throws Exception {
+        long tStart = System.currentTimeMillis();
+        new SimpleRunner(new MappedFileSource(args[0])).runWith(this);
+        long tMatch = System.currentTimeMillis() - tStart;
+        log.info("total time taken: {}s", (tMatch) / 1000.0);
+    }
+
+    public void runControlled(String[] args) throws Exception {
         String rep_pwd = args[0];
         String output = "features/" + rep_pwd.substring(rep_pwd.lastIndexOf("ds/") + 3, rep_pwd.lastIndexOf("s/") + 12) + "_unit_order_v2.csv";
         
@@ -164,27 +249,48 @@ public class Main {
         action_writer.write(sb.toString());
 
 
+        running = true;
+
         long tStart = System.currentTimeMillis();
-        new SimpleRunner(new MappedFileSource(args[0])).runWith(this);
+        ControllableRunner cRunner = new ControllableRunner(new MappedFileSource(args[0])).runWith(this);
+        for (int i = 0 ; i < 30000; i++) {
+            cRunner.tick();
+        }
+
+        if (ctx != null) {
+            Iterator<Entity> playerEntities = getEntities(ctx, "CDOTAPlayer");
+            while(playerEntities.hasNext()) {
+                Entity playerEntity = playerEntities.next();
+                if (isGamePlayer(playerEntity))
+                {
+                    ent_list.add(playerEntity);
+                    // int playerID = getEntityProperty(playerEntity, "m_iPlayerID");
+                    // System.out.println(playerID);
+                    // int heroID = resolveValue(ctx, "CDOTA_PlayerResource", "m_vecPlayerTeamData.%i.m_nSelectedHeroID", playerID, 0, 0);
+                    // System.out.println(heroID);
+                    // int selectedHero = resolveValue(ctx, "CDOTA_PlayerResource", "m_vecPlayerTeamData.%i.m_hSelectedHero", playerID, 0, 0);
+                    // System.out.println(selectedHero);
+                    // long steamID =  resolveValue(ctx, "CDOTA_PlayerResource", "m_vecPlayerData.%i.m_iPlayerSteamID", playerID, 0, 0);
+                    // System.out.println(steamID);
+                    // int teamNum = getEntityProperty(playerEntity, "m_iTeamNum");
+                    // System.out.println(teamNum);
+                }
+            }
+        }    
+
+        while(!cRunner.isAtEnd()) {
+            cRunner.tick();
+        }
+        cRunner.halt();
+
         long tMatch = System.currentTimeMillis() - tStart;
         log.info("total time taken: {}s", (tMatch) / 1000.0);
         action_writer.close();
     }
 
-    public void runControlled(String[] args) throws Exception {
-        long tStart = System.currentTimeMillis();
-        ControllableRunner runner = new ControllableRunner(new MappedFileSource(args[0])).runWith(this);
-        while(!runner.isAtEnd()) {
-            runner.tick();
-        }
-        long tMatch = System.currentTimeMillis() - tStart;
-        log.info("total time taken: {}s", (tMatch) / 1000.0);
-        
-    }
-
 
     public static void main(String[] args) throws Exception {
-        new Main().run(args);
+        new Main().runControlled(args);
     }
 
 }
