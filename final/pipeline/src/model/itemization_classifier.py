944c80e7f324c8dd522feab30ee4312bdba1b069
Sali <ucontacti2012@gmail.com>
2022-07-25 18:27:28 +0200

    itemization fixes

--- final/pipeline/src/model/itemization_classifier.py
+++ final/pipeline/src/model/itemization_classifier.py
@@ -12,7 +12,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import make_scorer, roc_curve #for eer
 from scipy.optimize import brentq #for eer
 from scipy.interpolate import interp1d #for eer
-
+import json
 import importlib
 spam_spec = importlib.util.find_spec("util.ESN")
 found_ESN = spam_spec is not None
@@ -26,6 +26,7 @@ REPLAY_TRACKER_PATH = "../resources/replay_tracker.csv"
 class ItemizationClassifier:
     def __init__(self, num_of_players) -> None:
         self.num_of_players = num_of_players
+        self.loaded_dict = json.load(open("../resources/item_converter.txt"))
         if found_ESN:
             self.__create_esn()
 
@@ -47,40 +48,44 @@ class ItemizationClassifier:
         
     def __create_esn(self) -> None:
         replay_tracker = pd.read_csv(REPLAY_TRACKER_PATH)
-        authentic_match_id = replay_tracker.loc[(replay_tracker['state'] == 6) & (replay_tracker['1_tick'] == True), 'replay_id'].tolist()
-
+        authentic_match_id = replay_tracker.loc[(replay_tracker['state'] == 7) & (replay_tracker['1_tick'] == True), 'replay_id'].tolist()
+        
         input = []
         self.steam_id_list = []
         for match_id in authentic_match_id:
-            #print(match_id)
             match_id = str(match_id)
-            path = "../automation/features/" + match_id + "_item_change_tmp.csv"
+            path = "../features/" + match_id + "_item_change_tmp.csv"
             if not os.path.isfile(path):
+                print("bruh")
                 continue
             df_item = pd.read_csv(path)
-            df_match_info = pd.read_csv("../automation/features/"  + match_id + "_info.csv")
+            df_item.fillna(0, inplace=True)
+            df_item.replace(self.loaded_dict, inplace=True)
+            df_match_info = pd.read_csv("../features/"  + match_id + "_info.csv")
             dfs_item = [rows for _, rows in df_item.groupby('Hero')]
             for item in dfs_item:
                 steam_id = df_match_info.loc[df_match_info["Hero"] == item["Hero"].iloc[0]].iloc[0]["SteamId"]
                 hero_name = df_match_info.loc[df_match_info["Hero"] == item["Hero"].iloc[0]].iloc[0]["Hero"]
-                input.append(np.array(item[["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9"]].values.tolist()))
+                temp_item = item[["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9"]].values.tolist()
+                input.append(temp_item)
                 self.steam_id_list.append(steam_id)
-        steam_id_list = np.array(self.steam_id_list)
+        self.steam_id_list = np.array(self.steam_id_list)
         X_TSObjs=[ESNTimeSeries(Xi) for Xi in input]
 
         numberOfReservoirNeurons=100
-        esnObj=ESNVanilla(numberOfReservoirNeurons,n=X_TSObjs[0].inputDim,saveLastHiddenState=False)
+        esnObj=ESNVanilla(numberOfReservoirNeurons,n=X_TSObjs[0].inputDim,saveLastHiddenState=True)
 
         self.X_H=[esnObj.getLastInternalStateEmbedding(X_TSObjs_i) for X_TSObjs_i in X_TSObjs]
+        
         self.X_H=np.array(self.X_H)
 
 
-    def __calculate_eer(y_true, y_score):
+    def __calculate_eer(self, y_true, y_score):
         fpr, tpr, thresholds = roc_curve(y_true, y_score, pos_label=1)
         eer = brentq(lambda x : 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
         return eer
     
-    def train_and_eval(self, target) -> None:
+    def train_and_eval(self) -> None:
         result_values = {}
         result_dict = {}
         result_dict["accuracy"] = []
@@ -90,10 +95,10 @@ class ItemizationClassifier:
         result_dict["roc"] = []
         result_dict["eer"] = []
         steamer = pd.DataFrame(self.steam_id_list).value_counts()
-        self.X_H = np.delete(self.X_H, np.where(~np.isin(self.steam_id_list, target)), axis = 0)
-        y = np.delete(self.steam_id_list, np.where(~np.isin(self.steam_id_list, target)))
+        self.X_H = np.delete(self.X_H, np.where(~np.isin(self.steam_id_list, steamer[:self.num_of_players].index.to_list())), axis = 0)
+        y = np.delete(self.steam_id_list, np.where(~np.isin(self.steam_id_list, steamer[:self.num_of_players].index.to_list())))
         self.X_H = preprocessing.StandardScaler().fit(self.X_H).transform(self.X_H)
-        for counter, player in enumerate(target):
+        for counter, player in enumerate(steamer[:self.num_of_players].index):
             new_y = np.where(y == player, 1, 0)
             result_rm = cross_validate(self.clf, self.X_H, new_y, cv = 5,  \
                         scoring={'precision': 'precision', 'recall': 'recall', 'accuracy': 'accuracy', \
@@ -116,9 +121,6 @@ class ItemizationClassifier:
         print("eer: " + str(sum(result_dict["eer"]) / len(result_dict["eer"])))
             
         # return result_dict, result_values
-
-    def __preprocess(self) -> None:
-        self.data = preprocessing.StandardScaler().fit(self.data).transform(self.data)
 
     def calculate_coefficiency():
         pass
